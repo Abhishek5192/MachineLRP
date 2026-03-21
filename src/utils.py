8d51dbf33e1c9c8a05263ff305eafd70f416276c
Abhishek5192 <abhisher27@gmail.com>
2026-03-21 13:33:25 +0530

    third commit

--- src/utils.py
+++ src/utils.py
@@ -2,19 +2,27 @@ import os
 import pickle
 import sys
 
+from sklearn.model_selection import GridSearchCV
+
 from src.logger import logging
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 import pandas as pd
 import dill
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
         models_list=list(models.values())
         model_keys=list(models.keys())
         for i in range(len(models)):
             model = models_list[i]
+           
+            para=param[model_keys[i]]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+           
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             y_test_pred = model.predict(X_test)
             test_model_score = r2_score(y_test, y_test_pred)
